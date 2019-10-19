d9828353c5bd24e0afb4ff07849970b28b6dbd7c
javict <6javierct93@gmail.com>
2019-10-19 20:46:27 +0100

    Select city

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function getCountries() {
+
+function getCountries(jQuery) {
     $.getJSON("data/countries.json", function(data) {
         var size = 0, key;
         for (key in data) {
@@ -13,18 +14,18 @@ function getCountries() {
             x.options.add(c, 1);
         };
     });
-    $.getJSON("data/cities.json", function(data) {
-        console.log(data["Afghanistan"]);
-        var aux = data["Afghanistan"]
+    // $.getJSON("data/cities.json", function(data) {
+    //     console.log(data["Afghanistan"]);
+    //     var aux = data["Afghanistan"]
 
-        for (var i = 0; i < data["Afghanistan"].lenght -1; i++) {
-            console.log(i);
+    //     for (var i = 0; i < data["Afghanistan"].lenght -1; i++) {
+    //         console.log(i);
             
-            var x = document.getElementById("selectCities");
-            var c = document.createElement("option");
-            var keys = Object.keys( data[1] );
-            c.text = keys[i];
-            x.options.add(c, 1);
-        };
-    });
+    //         var x = document.getElementById("selectCities");
+    //         var c = document.createElement("option");
+    //         var keys = Object.keys( data[1] );
+    //         c.text = keys[i];
+    //         x.options.add(c, 1);
+    //     };
+    // });
 }
