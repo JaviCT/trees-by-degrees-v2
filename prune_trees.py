25f3cf6372fcd9894b45ac62c5f96629f85fe5d8
Enric Moreu <enric.moreu.filella@gmail.com>
2019-10-20 10:36:51 +0100

    Sanfrancisco trees

--- prune_trees.py
+++ prune_trees.py
@@ -1,23 +1,21 @@
 import csv
 
+l1=37.784825
+l2=-122.490692
+l3=37.812361
+l4=-122.418938
+for line in open("./data/sanfrancisco.csv"):
+    csv_row = line.split(',')
+    if csv_row[15]=='':
+        continue
+    cord1=float(csv_row[15])
+    cord2=float(csv_row[16])
+    print(cord1,cord2)
+    if cord1> l1  and cord1 < l3 and cord2> l2  and cord2 < l4:
 
-l1=-0.113125
-l2=51.498084
-l3=-0.043430
-l4=51.519612
-for line in open("../data/london.csv"):
-    try:
-        csv_row = line.split(',')
-        cord1=float(csv_row[8])
-        cord2=float(csv_row[9])
-        if cord1> l1  and cord1 < l3 and cord2> l2  and cord2 < l4:
-            print(csv_row[8], csv_row[9])
-
-            with open('out.csv', 'a') as csvfile:
-                spamwriter = csv.writer(csvfile, delimiter=',')
-                spamwriter.writerow([
-                cord1,
-                cord2
-                ])
-    except:
-        print('oops')
+        with open('out2.csv', 'a') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',')
+            spamwriter.writerow([
+            cord1,
+            cord2
+            ])
